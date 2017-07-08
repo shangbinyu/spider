7a2f4652324b842e87fba8737196b91215f3c095
shangbinyu <shangbin_yu@163.com>
2017-07-08 13:32:10 +0800

    Update spride.py

--- spride.py
+++ spride.py
@@ -6,13 +6,21 @@ from redis_manager import RedisTermsManager
 class CrawlBSF:
         re_categories = re.compile('<a href="/fenlei/(.*?)"', re.S|re.M)
         base_url = 'https://baike.baidu.com/'
-        
+         request_headers = {
+             'host': "www.baidu.com",
+             'connection': "keep-alive",
+             'cache-control': "no-cache",
+             'upgrade-insecure-requests': "1",
+             'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36",
+             'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+             'accept-language': "zh-CN,en-US;q=0.8,en;q=0.6"
+        }
         redis_cls = RedisTermsManager()
         
         def getpagecontent(self):
                 print("downloading start")
                 try:
-                      response = urllib.request.urlopen(self.base_url,data = None, timeout = 10)
+                      response = urllib.request.urlopen(self.base_url,data = None, headers=self.request_headers,timeout = 10)
                       html_page = response.read()
                 except Exception as e:
                       return e
