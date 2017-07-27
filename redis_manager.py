ebd3739ff060a3dd4771cf1b4eb105cc986a2040
shangbinyu <shangbin_yu@163.com>
2017-07-27 23:59:31 +0800

    20170727
    aaa

--- redis_manager.py
+++ redis_manager.py
@@ -9,16 +9,11 @@ import redis
 class RedisTermsManager:
     def __init__(self, sever_ip='localhost', client=None, expires=timedelta(days=30)):
         self.redis_client = redis.StrictRedis(host=sever_ip, port=6379, db=0)
-
     # Insert a new item into a collection specified by queue_name
-    def enqueue_item(self, queue_name, item):
-        num = self.redis_client.get(queue_name + item)
-        if num is not None:
-            self.redis_client.set(queue_name + item, int(num) + 1 )
-            return num
-        self.redis_client.set(queue_name + item, 1)
-        self.redis_client.lpush(queue_name, item)
+    def enqueue_item(self,item):
+        num = self.redis_client.get(item)
+        if num is None:
+            self.redis_client.set(item, 0)
         return 0
-
     def clear(self):
         self.redis_client.flushall()
--- spride.py
+++ spride.py
@@ -3,36 +3,54 @@ import urllib.request
 import http.client
 import re
 from redis_manager import RedisTermsManager
+import time
+
 class CrawlBSF:
-        re_categories = re.compile('<a href="/fenlei/(.*?)"', re.S|re.M)
-        base_url = 'https://baike.baidu.com/'
+        re_seed = re.compile('<h5 class="feed-ver-title"><a target="_blank" href="(.*?)"', re.S | re.M)
+        re_detail = re.compile('<h5 class="feed-ver-title"><a target="_blank" href="(.*?)"', re.S | re.M)
+        base_url = 'http://faxian.smzdm.com/'
         request_headers = {
-             'host': "www.baidu.com",
+             'host': "faxian.smzdm.com",
              'connection': "keep-alive",
              'cache-control': "no-cache",
              'upgrade-insecure-requests': "1",
-             'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36",
+             'user-agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3095.5 Safari/537.36",
              'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-             'accept-language': "zh-CN,en-US;q=0.8,en;q=0.6"
+             'accept-language': "zh-CN,zh;q=0.8"
         }
         redis_cls = RedisTermsManager()
-        
+
+        def getpagedetail(self,url):
+            try:
+                item_detail_page = urllib.request.Request(url=url, headers=self.request_headers)
+                f_item_detail = urllib.request.urlopen(item_detail_page)
+                html_detail_page = f_item_detail.read().decode('utf-8')
+            except Exception as e:
+                return e
+            item_deatail = self.re_seed.findall(html_detail_page)
+
         def getpagecontent(self):
                 print("downloading start")
                 try:
-                      response = urllib.request.urlopen(self.base_url,data = None,timeout = 10)
-                      html_page = response.read()
+                      response = urllib.request.Request(url=self.base_url, headers=self.request_headers)
+                      print("downloading start")
+                      f = urllib.request.urlopen(response)
+                      html_page = f.read().decode('utf-8')
                 except Exception as e:
                       return e
                 print("downloading read")
-                html_page = html_page.decode('utf-8')
-                categories = self.re_categories.findall(html_page)
-                filename = "baidu_baike_categories.html"
-                fo = open( filename, 'wb+')
-                for category in categories:
-                        fo.write(str.encode(category))
-                        self.redis_cls.enqueue_item(self,"spider",str.encode(category))
-                fo.close()
+                item_urls = self.re_seed.findall(html_page)
+                for item in item_urls:
+                        print(item)
+                        self.redis_cls.enqueue_item(item)
                 print("downloading end")
+
 crawler = CrawlBSF()
-print(crawler.getpagecontent())
+for num in range(1,10):
+    print("Start:%s" % time.ctime())
+    time.sleep(20)
+    crawler.getpagecontent()
+    print("End:%s" % time.ctime())
+
+
+
